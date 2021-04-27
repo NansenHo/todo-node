c10a1cf871d231b7aad7b08c127060cc961b034f
henansong <github@nansenho.com>
2021-04-27 11:13:30 +0800

    task_1

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const program = require('commander')
 
 program
     .option('-x, --xxx', 'what the x')
+program
+    .command('add')
+    .description('add a task')
+    .action((...args) => {
+        const task = args.slice(0, -1).join(' ')
+        console.log(task);
+    });
 
 program.parse(process.argv);
 
