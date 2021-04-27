accf0c49255964b84bb543b75deefa779352e762
henansong <github@nansenho.com>
2021-04-27 15:17:06 +0800

    ç读取到之前的任务

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const homedir = require('os').homedir();
+const home = process.env.HOME || homedir;
+const p = require('path') // node.js 提供的专门用来拼接路径的
+const dbPath = p.join(home, '.todo')
+const fs = require('fs')
+
 module.exports.add = (task) => {
-    console.log('hi');
+    fs.readFile(dbPath, {flag: 'a+'}, (error, data)=>{
+        console.log(data.toString())
+    })
 }
+
