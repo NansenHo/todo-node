cc221791c53374a007ca07d86ac29c6100fb0307
henansong <github@nansenho.com>
2021-04-27 11:48:25 +0800

    rename and add index.js

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-const program = require('commander')
-
-program
-    .option('-x, --xxx', 'what the x')
-program
-    .command('add')
-    .description('add a task')
-    .action((...args) => {
-        const task = args.slice(0, -1).join(' ')
-        console.log(task);
-    });
-program
-    .command('clear')
-    .description('clear all tasks')
-    .action((...args) => {
-        console.log('this is clear');
-    });
-
-program.parse(process.argv);
-
-console.log(program.xxx);
+module.exports.add = (task) => {
+    console.log('hi');
+}
