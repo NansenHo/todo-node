9f7bc961c6f5542e11905009ae0424e5fbfae6d9
henansong <github@nansenho.com>
2021-04-28 11:34:24 +0800

    添加简单 readme，删掉多余的依赖

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// 获取到用户的 home 目录
-const homedir = require('os').homedir(); //// nodejs 获取用户的 home 目录
-const home = process.env.HOME || homedir; //// nodejs 获取系统的环境变量
-const p = require('path') //// path 是 nodejs 提供的专门用来将字符串拼接成路径的方法
-const dbPath = p.join(home, '/.todo')
-const fs = require('fs')
 const db = require('./db.js')
 
 module.exports.add = async (task) => {
