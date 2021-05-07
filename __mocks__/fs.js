f2a301fecb01c34a9d1bcd7b450c48e20d42ba99
henansong <github@nansenho.com>
2021-05-07 10:52:29 +0800

    unit test - write file

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -3,18 +3,37 @@ const _fs = jest.requireActual('fs') // _fs 是真正的 fs
 
 Object.assign(fs, _fs)
 
-const mocks = {}
+const readMocks = {}
 
-fs.setMock = (path, error, data) => {
-    mocks[path] = [error, data]
+fs.setReadFileMock = (path, error, data) => {
+    readMocks[path] = [error, data]
 }
 
 fs.readFile = (path, options, callback) => {
-    if (callback === undefined) { callback = options }
-    if (path in mocks) {
-        callback(...mocks[path]) // error 和 data
+    if (callback === undefined) {
+        callback = options
+    }
+    if (path in readMocks) {
+        callback(...readMocks[path]) // error 和 data
     } else {
         _fs.readFile(path, options, callback)
+    }
+}
+
+const writeMocks = {}
+
+fs.setWriteFileMock = (path, fn) => {
+    writeMocks[path] = fn
+}
+
+fs.writeFile = (path, data, options, callback) => {
+    if (callback === undefined) {
+        callback = options
+    }
+    if (path in writeMocks) {
+        writeMocks[path](path, data, options, callback)
+    } else {
+        _fs.writeFile(path, data, options, callback)
     }
 }
 
--- __tests__/db.spec.js
+++ __tests__/db.spec.js
@@ -2,12 +2,22 @@ const db = require("../db.js")
 const fs = require("fs")
 jest.mock('fs')
 
-describe("db", ()=>{
-    it("can read", async ()=>{
+describe("db", () => {
+    it("can read", async () => {
         const data = [{title: 'hi', done: true}]
-        fs.setMock('/xxx', null, JSON.stringify(data))
+        fs.setReadFileMock('/xxx', null, JSON.stringify(data))
         const list = await db.read("/xxx")
         expect(list).toStrictEqual(data) // 要对比两个对象要用 toStrictEqual
+    })
+    it('can write', async () => {
+        let fakeFile
+        fs.setWriteFileMock('/yyy', (path, data, callback) => {
+            fakeFile = data
+            callback(null)
+        })
+        const list = [{title: 'read', done: true}, {title: 'write', done: false}]
+        await db.write(list, '/yyy')
+        expect(fakeFile).toBe(JSON.stringify(list) + '\n')
     })
 })
 
